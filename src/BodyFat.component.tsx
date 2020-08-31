d959c07ff22be23f3c5647478328c39a77ea1d40
Ali Mert <mail@alimert.net>
2020-08-31 23:55:46 +0300

    Update BodyFat.component.tsx

--- src/BodyFat.component.tsx
+++ src/BodyFat.component.tsx
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import HumanIcon from "./Svg/HumanIcon.svg";
 
+//TODO humanIcon'un için yağ ile doldurmayı unutma
 const useStyles = makeStyles({
   root: {
     height: 300,
