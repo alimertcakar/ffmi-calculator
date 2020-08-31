7fd25d015275b3c64e71a09acf347a5b8cc670b9
Ali Mert <mail@alimert.net>
2020-08-31 10:17:38 +0300

    up

--- src/BodyParts/Head.component.tsx
+++ src/BodyParts/Head.component.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import Headsvg from "../Svg/head.svg";
+import Grid from "@material-ui/core/Grid";
+import { motion } from "framer-motion";
 
 function App() {
-  return <img src={Headsvg} alt=""></img>;
+  return (
+    <motion.div
+      initial={{ scale: 0 }}
+      animate={{ scale: 2 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      }}
+    >
+      <Grid container justify="center">
+        <img
+          src={Headsvg}
+          alt=""
+          height="90px"
+          width="50%"
+          style={{ marginBottom: "10px" }}
+        ></img>
+      </Grid>
+    </motion.div>
+  );
 }
 
 export default App;
